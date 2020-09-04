99b032a4fa2de573aeefced69005d279b7c25992
sinanermis <sinann.ermis@gmail.com>
2020-09-05 00:07:47 +0300

    feat: Pools added

---
+++ Assets/Scripts/Toolkit/GameObjectPool.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rhodos.Toolkit
+{
+    public class GameObjectPool
+    {
+        private readonly GameObject _prefab;
+        private readonly List<GameObject> _objectPool = new List<GameObject>();
+
+        public GameObjectPool(GameObject prefab)
+        {
+            _prefab = prefab;
+        }
+
+        public void LoadPool(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                GameObject gameObject = Object.Instantiate(_prefab);
+                AddObject(gameObject);
+            }
+        }
+
+        public GameObject[] GetAll()
+        {
+            return _objectPool.ToArray();
+        }
+
+        public GameObject GetObject()
+        {
+            if (_objectPool.Count > 0)
+            {
+                GameObject gameObject = _objectPool[_objectPool.Count - 1];
+                _objectPool.RemoveAt(_objectPool.Count - 1);
+                gameObject.SetActive(true);
+
+                return gameObject;
+            }
+
+            return Object.Instantiate(_prefab);
+        }
+
+        public GameObject GetObjectWithoutActivating()
+        {
+            if (_objectPool.Count > 0)
+            {
+                GameObject gameObject = _objectPool[_objectPool.Count - 1];
+                _objectPool.RemoveAt(_objectPool.Count - 1);
+
+                return gameObject;
+            }
+
+            return Object.Instantiate(_prefab);
+        }
+
+        public void AddObject(GameObject gameObject)
+        {
+            gameObject.SetActive(false);
+            _objectPool.Insert(0, gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Toolkit/MonoBehaviourPool.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rhodos.Toolkit
+{
+    public class MonoBehaviourPool<T> where T : MonoBehaviour
+    {
+        private readonly T _prefab;
+        private readonly List<T> _objectPool = new List<T>();
+
+
+        public MonoBehaviourPool(T prefab)
+        {
+            _prefab = prefab;
+        }
+
+        public void LoadPool(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                T gameObject = Object.Instantiate(_prefab);
+                AddObject(gameObject);
+            }
+        }
+
+        public T[] GetAll()
+        {
+            return _objectPool.ToArray();
+        }
+
+        public T GetObject()
+        {
+            if (_objectPool.Count > 0)
+            {
+                T gameObject = _objectPool[_objectPool.Count - 1];
+                _objectPool.RemoveAt(_objectPool.Count - 1);
+                gameObject.gameObject.SetActive(true);
+
+                return gameObject;
+            }
+
+            return Object.Instantiate(_prefab);
+        }
+
+        public T GetObjectWithoutActivating()
+        {
+            if (_objectPool.Count > 0)
+            {
+                T gameObject = _objectPool[_objectPool.Count - 1];
+                _objectPool.RemoveAt(_objectPool.Count - 1);
+
+                return gameObject;
+            }
+
+            return Object.Instantiate(_prefab);
+        }
+
+        public void AddObject(T gameObject)
+        {
+            gameObject.gameObject.SetActive(false);
+            _objectPool.Insert(0, gameObject);
+        }
+
+    }
+}
