3c4603ed6d01605d6170c7fa3429fe2afff53547
RhodosTheGod <sinann.ermis@gmail.com>
2020-11-17 12:52:01 +0300

    DORoutine added into project

---
+++ Assets/Scripts/Toolkit/DORoutine/CoroutineCore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DORoutine
+{
+    public static class CoroutineCore
+    {
+        public static CoroutineHandler CoroutineHandler { get; }
+
+        static CoroutineCore()
+        {
+            GameObject go = new GameObject("Coroutine Handler");
+            CoroutineHandler = go.AddComponent<CoroutineHandler>();
+            go.hideFlags = HideFlags.HideInHierarchy;
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/Toolkit/DORoutine/Sequence.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DORoutine
+{
+    public class Sequence
+    {
+        private Coroutine _coroutine = default;
+        private IEnumerator Delayer(float duration)
+        {
+            yield return new WaitForSecondsRealtime(duration);
+        }
+        
+        #region Append Methods
+
+        public Sequence Append(IEnumerator enumerator)
+        {
+            _coroutine = _coroutine.StartNext(enumerator);
+            return this;
+        }
+
+        public Sequence Append(Coroutine enumerator)
+        {
+            _coroutine = _coroutine.StartNext(enumerator);
+            return this;
+        }
+
+        public Sequence AppendCallback(Action callback)
+        {
+            _coroutine = _coroutine.OnComplete(callback);
+            return this;
+        }
+
+        public Sequence AppendInterval(float duration)
+        {
+            _coroutine = _coroutine.StartNext(Delayer(duration));
+            return this;
+        }
+
+        #endregion
+
+        #region Prepend Methods
+
+
+        public Sequence Prepend(IEnumerator enumerator)
+        {
+            _coroutine = enumerator.StartCoroutine().StartNext(_coroutine);
+            return this;
+        }
+
+        public Sequence Prepend(Coroutine enumerator)
+        {
+            _coroutine = enumerator.StartNext(_coroutine);
+            return this;
+        }
+
+        public Sequence PrependCallback(Action callback)
+        {
+            _coroutine = _coroutine.OnStart(callback);
+            return this;
+        }
+        
+        public Sequence PrependInterval(float duration)
+        {
+            _coroutine = Delayer(duration).StartCoroutine().StartNext(_coroutine);
+            return this;
+        }
+
+        #endregion
+
+        public static implicit operator Coroutine(Sequence sequence)
+        {
+            return sequence._coroutine;
+        }
+    }
+}
