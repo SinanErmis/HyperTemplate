ba06fdecfa9f5c589c48f9af98d74b2ea6ef1937
Sinan Ermiş <sinann.ermis@gmail.com>
2021-08-17 13:24:05 +0300

    Add isCumulative option to InstantVectorSwipe

--- Assets/Scripts/Mechanics/Bases/InstantVectorSwipeMechanic.cs
+++ Assets/Scripts/Mechanics/Bases/InstantVectorSwipeMechanic.cs
@@ -4,9 +4,14 @@ namespace Rhodos.Mechanics.Bases
 {
     /// <summary>
     /// A base mechanic class that triggers SwipeAction with a vector2 in every frame that drag continues.
+    /// 
     /// </summary>
     public abstract class InstantVectorSwipeMechanic : Mechanic
     {
+        [Header("Mechanic Settings")] 
+        [Tooltip("If true, SwipeAction will be triggered with an Vector2 which is accumulated since touch starts")]
+        [SerializeField] private bool isCumulative;
+        
         private Vector2 _firstPos;
         public override void OnDown()
         {
@@ -15,6 +20,7 @@ namespace Rhodos.Mechanics.Bases
         public override void OnDrag()
         {
             SwipeAction((Vector2) Input.mousePosition - _firstPos);
+            if (!isCumulative) _firstPos = Input.mouseScrollDelta;
         }
         
         protected abstract void SwipeAction(Vector2 swipe);
